feabf29b5398ae1c0cff676154bcdac6657904a5
Birol Köklü <birolkoklu@gmail.com>
2019-02-12 22:48:38 +0300

    Proje Hakkında sayfası düzenlendi.

--- src/views/Info.vue
+++ src/views/Info.vue
@@ -7,6 +7,18 @@
 
     <p class="info__description">Stillendirme işlemleri SASS ile hazırlanıp, BEM metolojisi uygulanmıştır.</p>
 
+    <h1 class="info__title">Şehirler Hakkında</h1>
+
+    <p class="info__description">İl seçimleri, sevdiğim müzisyenler ve adı müzik ile bütünleşmiş şehirler düşünülerek seçildi.</p>
+
+    <p class="info__description">
+      Seattle - Kurt Cobain<br />
+      Bethel - Woodstock<br />
+      Teoc - Mississippi John Hurt<br />
+      Salzburg - Mozart<br />
+      Kırşehir - Neşet Ertaş
+    </p>
+
     <h1 class="info__title">Geliştirici Hakkında</h1>
 
     <p class="info__description">2014'ten beri hem freelance hem remote hem de tam zamanlı olarak bir çok firma ile çalışmalar yaptım. 2018 yılının Ocak ayında kendi şirketimi kurdum, bir yıldır remote çalışarak bir çok firmaya projeler geliştirdim.</p>
